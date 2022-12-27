effc0a65a87d056b367aa6dabcf66da8c11f8d2d
ChaseRensberger <chasehrensberger@gmail.com>
2022-12-27 02:07:12 -0500

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Card from '../components/Card';
+
+const Home = () => {
+	return (
+		<>
+			<div className="flex-row justify-center items-center flex">
+				<Card></Card>
+			</div>
+		</>
+	);
+};
+
+export default Home;
